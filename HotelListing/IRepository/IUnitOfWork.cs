581e974b5757eb856108fc1559a48222f57c0d61
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-04 19:49:00 -0300

    feature: set up service repositories and dependency injection

---
+++ HotelListing/IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using HotelListing.Data;
+using HotelListing.IRepository;
+using System.Threading.Tasks;
+
+namespace HotelListing.Repository
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<Country> Coutries { get; }
+
+        IGenericRepository<Hotel> Hotels { get; }
+
+        Task Save();
+    }
+}

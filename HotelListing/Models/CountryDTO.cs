80576ab96270fccf212cdb2f85807f3ea5471fd5
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-05 14:43:34 -0300

    feat: added country dto

---
+++ HotelListing/Models/CountryDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.Models
+{
+    public class CreateCountryDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 50, ErrorMessage = "O Nome é longo demais")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 2, ErrorMessage = "O Nome é longo demais")]
+        public string ShortName { get; set; }
+    }
+
+    public class CountryDTO : CreateCountryDTO
+    {
+        public int Id { get; set; }
+        public virtual IList<HotelDTO> Hotels { get; set; }
+
+
+    }
+}

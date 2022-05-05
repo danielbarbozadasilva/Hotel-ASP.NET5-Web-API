26b819d5d783373325b52aeaa36419c17ce7b268
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-05 15:24:48 -0300

    fix: rename countries interface

--- HotelListing/IRepository/IUnitOfWork.cs
+++ HotelListing/IRepository/IUnitOfWork.cs
@@ -6,7 +6,7 @@ namespace HotelListing.Repository
 {
     public interface IUnitOfWork
     {
-        IGenericRepository<Country> Coutries { get; }
+        IGenericRepository<Country> Countries { get; }
 
         IGenericRepository<Hotel> Hotels { get; }
 

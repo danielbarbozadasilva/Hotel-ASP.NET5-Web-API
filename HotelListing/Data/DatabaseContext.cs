6fd899e20a5f1aae3449c53ef45d89561932e7ab
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-07 22:44:15 -0300

    refactor: optimize the way to populate db

--- HotelListing/Data/DatabaseContext.cs
+++ HotelListing/Data/DatabaseContext.cs
@@ -1,67 +1,25 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HotelListing.Configurations.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.Data
 {
-    public class DatabaseContext : DbContext
+    public class DatabaseContext : IdentityDbContext<ApiUser>
     {
         public DatabaseContext(DbContextOptions options) : base(options)
-        {
+        { }
 
-        }
 
         public DbSet<Country> Countries { get; set; }
         public DbSet<Hotel> Hotels { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Country>().HasData(
-                new Country
-                {
-                    Id = 1,
-                    Name = "Brasil",
-                    ShortName = "BR"
-                },
-                new Country
-                {
-                    Id = 2,
-                    Name = "Argentina",
-                    ShortName = "ARG"
-                },
-                new Country
-                {
-                    Id = 3,
-                    Name = "Jamaica",
-                    ShortName = "JM"
-                }
-            );
-            builder.Entity<Hotel>().HasData(
-               new Hotel
-               {
-                   Id = 1,
-                   Name = "Hotel Resort abc",
-                   Address = "Rua abc",
-                   CountryId = 1,
-                   Rating = 4.6
-               },
-               new Hotel
-               {
-                   Id = 2,
-                   Name = "Hotel avdvd",
-                   Address = "Rua abc",
-                   CountryId = 2,
-                   Rating = 4.8
-               },
-               new Hotel
-               {
-                   Id = 3,
-                   Name = "Hotel abc",
-                   Address = "Rua abc",
-                   CountryId = 3,
-                   Rating = 3.9
-               }
-           );
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new CountryConfiguration());
+            builder.ApplyConfiguration(new HotelConfiguration());
+            builder.ApplyConfiguration(new RoleConfiguration());
         }
-
-
     }
 }

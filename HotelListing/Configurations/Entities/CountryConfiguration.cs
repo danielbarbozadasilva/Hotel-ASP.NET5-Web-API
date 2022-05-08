4ea383cd24f3fae80a69d72f1477b0fc3e727a1c
Daniel Barboza da SIlva <daniel80barboza@gmail.com>
2022-05-07 22:41:14 -0300

    feature: added country configuration

---
+++ HotelListing/Configurations/Entities/CountryConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.Configurations.Entities
+{
+    public class CountryConfiguration : IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.HasData(
+                new Country
+                {
+                   Id = 1,
+                   Name = "Brasil",
+                   ShortName = "BR"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Argentina",
+                    ShortName = "ARG"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                }
+            );
+        }
+    }
+}
